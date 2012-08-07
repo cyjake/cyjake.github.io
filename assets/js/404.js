793ae6fa910a45f67f3b89b2774f16f8fe648d8e
Jake Chen <jakeplus@gmail.com>
2012-08-07 16:51:22 +0800

    Fixed guessing original url logic

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,17 +1,18 @@
 (function(undefined) {
 
     var doc = document,
-        referer = doc.referer,
+        referer = doc.referer || location.href,
         dispatcher = doc.getElementById('dispatcher'),
         cands = dispatcher.getElementsByTagName('a'),
         path, i, a, len;
 
     if (referer && /cyj.me/.test(referer)) {
-        path = referer.replace(/http:\/\/([^.]+\.)?cyj.me/, '');
+        path = referer.replace(/http:\/\/([^.]+\.)?cyj.me\//, '');
         if (path) {
             for (i = 0, len = cands.length; i < len; i++) {
+                a = cands[i];
                 a.href += path;
-                a.innerHTML += path;
+                a.innerHTML += '/' + path;
             }
         }
     }
