620efb10f1aec099ee83808b012cf2fc8098c418
Jake Chen <jakeplus@gmail.com>
2012-08-07 16:46:37 +0800

    Added 404 page

---
+++ assets/js/404.js
@@ -0,0 +1,21 @@
+(function(undefined) {
+
+    var doc = document,
+        referer = doc.referer,
+        dispatcher = doc.getElementById('dispatcher'),
+        cands = dispatcher.getElementsByTagName('a'),
+        path, i, a, len;
+
+    if (referer && /cyj.me/.test(referer)) {
+        path = referer.replace(/http:\/\/([^.]+\.)?cyj.me/, '');
+        if (path) {
+            for (i = 0, len = cands.length; i < len; i++) {
+                a.href += path;
+                a.innerHTML += path;
+            }
+        }
+    }
+    else {
+        dispatcher.style.display = 'none';
+    }
+})();
