3050b6444552e9291d54489b087e64427f049556
Chen Yangjian <252317+cyjake@users.noreply.github.com>
2026-09-13 20:56:24 +0800

    Refresh homepage photo gallery

--- assets/js/front.js
+++ assets/js/front.js
@@ -3,91 +3,75 @@
   'use strict'
 
   var gallery = [
-    { title: '',
-      asset: '/assets/gallery/village.jpg',
-      url: 'https://img.alicdn.com/tfscom/TB1xDG5lQvoK1RjSZFDXXXY3pXa.jpg' }
+    {
+      title: '',
+      src: '/assets/gallery/village-1280.webp',
+      srcset: '/assets/gallery/village-1280.webp 1280w, /assets/gallery/village-2560.webp 2560w',
+      tone: '#333640',
+      theme: 'light'
+    },
+    {
+      title: 'July 2025',
+      src: '/assets/gallery/2025-07-20-r0001998-1280.webp',
+      srcset: '/assets/gallery/2025-07-20-r0001998-1280.webp 1280w, /assets/gallery/2025-07-20-r0001998-2560.webp 2560w',
+      tone: '#94b8d4',
+      theme: 'dark'
+    },
+    {
+      title: 'July 2020',
+      src: '/assets/gallery/2020-07-13-dsc00639-1280.webp',
+      srcset: '/assets/gallery/2020-07-13-dsc00639-1280.webp 1280w, /assets/gallery/2020-07-13-dsc00639-2560.webp 2560w',
+      tone: '#7c98ba',
+      theme: 'dark'
+    },
+    {
+      title: 'October 2025',
+      src: '/assets/gallery/2025-10-03-r0002395-1280.webp',
+      srcset: '/assets/gallery/2025-10-03-r0002395-1280.webp 1280w, /assets/gallery/2025-10-03-r0002395-2560.webp 2560w',
+      tone: '#c0c8d0',
+      theme: 'dark'
+    }
   ]
 
-  /**
-   * the interval of the gallery slide show.
-   */
   var GALLERY_INTERVAL = 10000
-
-  function elastic() {
-    var bg = document.querySelector('figure')
-    var img = document.querySelector('figure img')
-
-    var actualRatio = bg.offsetWidth / bg.offsetHeight
-    var expectedRatio = img.offsetWidth / img.offsetHeight
-    var offsetTop = 0
-    var offsetLeft = 0
-
-    if (actualRatio >= expectedRatio) {
-      img.style.maxWidth = '100%'
-      img.style.maxHeight = 'none'
-      offsetTop = (img.offsetWidth / actualRatio - img.offsetHeight) / 2
-      if (offsetTop < 0) {
-        img.style.marginTop = offsetTop + 'px'
-        img.style.marginLeft = 0
-      }
-    } else {
-      img.style.maxHeight = '100%'
-      img.style.maxWidth = 'none'
-      offsetLeft = (img.offsetHeight * actualRatio - img.offsetWidth) / 2
-      if (offsetLeft < 0) {
-        img.style.marginLeft = offsetLeft + 'px'
-        img.style.marginTop = 0
-      }
-    }
-  }
-
-  function throttle(fn, wait) {
-    var timer
-
-    return function() {
-      if (timer) return
-
-      timer = setTimeout(function() {
-        fn()
-        timer = null
-      }, wait)
-    }
-  }
-
+  var reduceMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches
+  var image = document.querySelector('figure img')
+  var caption = document.querySelector('figure figcaption')
   var loader = new Image()
-
-  function load(index, fn) {
-    var data = gallery[index]
-    var img = document.querySelector('figure img')
-    var caption =  document.querySelector('figure figcaption')
-
-    loader.onload = function onload() {
-      img.src = data.url
-      caption.innerHTML = data.title || 'Untitled'
-      elastic()
-      fn()
-      setTimeout(function() {
-        img.style.opacity = 1
-      }, 0)
-    }
-    loader.src = data.url
-    img.style.opacity = 0
-  }
-
   var galleryIndex = 0
 
+  function load(index) {
+    var data = gallery[index]
+
+    document.body.style.setProperty('--gallery-tone', data.tone)
+    document.body.dataset.theme = data.theme
+    image.style.opacity = 0
+
+    loader.onload = function onload() {
+      image.src = data.src
+      image.srcset = data.srcset
+      image.sizes = '100vw'
+      caption.textContent = data.title
+
+      window.requestAnimationFrame(function() {
+        image.style.opacity = 1
+      })
+
+      if (!reduceMotion && gallery.length > 1) {
+        window.setTimeout(next, GALLERY_INTERVAL)
+      }
+    }
+
+    loader.sizes = '100vw'
+    loader.srcset = data.srcset
+    loader.src = data.src
+  }
+
   function next() {
-    if (++galleryIndex >= gallery.length) galleryIndex = 0
-    load(galleryIndex, function() {
-      if (gallery.length > 1) setTimeout(next, GALLERY_INTERVAL)
-    })
+    galleryIndex = (galleryIndex + 1) % gallery.length
+    load(galleryIndex)
   }
 
-  function main() {
-    next()
-    window.addEventListener('resize', throttle(elastic, 100))
-  }
-
-  main()
+  load(galleryIndex)
 
 })()
